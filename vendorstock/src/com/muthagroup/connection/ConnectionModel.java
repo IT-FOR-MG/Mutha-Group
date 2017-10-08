4faf73980e6b02a6a659387ed157b46d0be73ef3
itsupports <itsupports@localhost>
2017-10-08 15:43:25 +0530

    tst

--- vendorstock/src/com/muthagroup/connection/ConnectionModel.java
+++ vendorstock/src/com/muthagroup/connection/ConnectionModel.java
@@ -12,7 +12,6 @@ public static Connection getConnection1()
 try{
 	Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 	con1 = DriverManager.getConnection("jdbc:sqlserver://192.168.0.6:1433;databaseName=FOUNDRYERP;user=UDAY;password=12345");	
-   
 	
 }
 catch(Exception e)
